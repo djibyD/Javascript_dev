b12c0c89b646bb659e1defcff261f90dc840443b
djiby <djiby@debian>
2017-10-16 22:01:42 +0200

    pocessing form

---
+++ inscription.js
@@ -0,0 +1,11 @@
+//Processing the form validation
+
+function validerName(){
+  var nom = document.getElementsByName('nom');
+  console.log("nom : " + document.getElementsByName('nom').value);
+}
+
+//Retourne le dernier element focus
+function getPreviousFocussedElement(){
+  var previousElement = document.activeElement;
+}
